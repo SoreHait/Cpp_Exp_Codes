b9146d662087be93de668eda52ab257a47a988e9
SoreHait <sorehait@foxmail.com>
2023-06-01 18:28:09 +0800

    upd

--- Exp6_1/main.cpp
+++ Exp6_1/main.cpp
@@ -42,7 +42,7 @@ Date Date::operator-(const int d) {
     day -= d;
     while (day < 1) {
         month--;
-        if (month < 12) {
+        if (month < 1) {
             month += 12;
             year--;
         }
@@ -52,7 +52,17 @@ Date Date::operator-(const int d) {
 }
 
 int Date::operator-(const Date& d) {
+    int y2, m2, d2;
+    int y1, m1, d1;
+    m1 = (d.month + 9) % 12;
+    y1 = d.year - m1/10;
+    d1 = 365*y1 + y1/4 - y1/100 + y1/400 + (m1*306 + 5)/10 + (d.day - 1);
 
+    m2 = (month + 9) % 12;
+    y2 = year - m2/10;
+    d2 = 365*y2 + y2/4 - y2/100 + y2/400 + (m2*306 + 5)/10 + (day - 1);
+
+    return (d2 - d1);
 }
 
 ostream &operator<<(ostream& output, const Date& d) {
@@ -68,6 +78,10 @@ istream &operator>>(istream& input, Date& d) {
 }
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    Date d1, d2;
+    cin >> d1 >> d2;
+    cout << d1 << endl << d2 << endl;
+    cout << d2 - d1 << endl;
+    cout << d1 - 72 << endl;
+    cout << d2 + 72 << endl;
 }
